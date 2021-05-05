c85b3f772c0302d51aad6bb8b54ea4dd684fc6e5
davi-santos5 <dbs.tj0@gmail.com>
2021-05-05 10:42:04 -0300

    update myProfile form

--- src/pages/myProfile.tsx
+++ src/pages/myProfile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import Image from "next/image";
 
 import { BannerMenu } from "../components/BannerMenu";
@@ -6,12 +6,36 @@ import { BannerMenu } from "../components/BannerMenu";
 import styles from "../styles/myProfile.module.css";
 
 export default function MyProfile() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [isPasswordEqual, setIsPasswordEqual] = useState(true);
+
+  useEffect(() => {
+    setIsPasswordEqual(password === confirmPassword);
+  }, [password, confirmPassword]);
+
+  const clearInputs = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  const handleSubmitChanges = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    if (isPasswordEqual) clearInputs();
+  };
+
   return (
     <div className={styles.container}>
       <BannerMenu />
       <div className={styles.wrapper}>
         <h1>/Dados Pessoais</h1>
-        <form>
+        <form onSubmit={handleSubmitChanges}>
           <label htmlFor="profilePicture">
             <Image
               width={39}
@@ -27,10 +51,50 @@ export default function MyProfile() {
             id="profilePicture"
             name="profilePicture"
           />
-          <input type="text" placeholder="Nome" />
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <input type="password" placeholder="Confirmar Senha" />
+          <input
+            type="text"
+            placeholder="Nome"
+            title="Insira seu novo nome"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            title="Insira seu novo e-mail"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            title="Insira sua nova senha"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Confirmar Senha"
+            title="Confirme sua nova senha"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            required
+            className={isPasswordEqual ? "" : styles.passwordWrong}
+          />
+          {!isPasswordEqual && (
+            <label htmlFor="confirmPassword">A senha não confere</label>
+          )}
           <button type="submit">Editar</button>
         </form>
       </div>
